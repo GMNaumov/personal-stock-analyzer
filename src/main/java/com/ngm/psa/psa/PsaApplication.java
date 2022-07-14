421b58732d122871d34c9ab83a34f2a257a3338e
username <username@1600x-vm1.localdomain>
2022-07-14 21:38:12 +0300

    Create first class

--- src/main/java/com/ngm/psa/psa/PsaApplication.java
+++ src/main/java/com/ngm/psa/psa/PsaApplication.java
@@ -9,5 +9,4 @@ public class PsaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PsaApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/ngm/psa/psa/Stock.java
@@ -0,0 +1,10 @@
+package com.ngm.psa.psa;
+
+import lombok.Data;
+
+@Data
+public class Stock {
+    private String ticker;
+    private String fullname;
+    private double price;
+}
